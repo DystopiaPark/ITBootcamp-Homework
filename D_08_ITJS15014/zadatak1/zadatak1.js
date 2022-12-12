2c7eb7420860c85d9ca3f155e8c469277694d929
Filip <68464364+DystopiaPark@users.noreply.github.com>
2022-12-12 17:56:04 +0100

    ispravka 8. domaceg

--- D_08_ITJS15014/zadatak1/zadatak1.js
+++ D_08_ITJS15014/zadatak1/zadatak1.js
@@ -1,11 +1,11 @@
-let v = 300;
-let n = 105;
+let v = 302;
+let n = 108;
 if (v / n >= 3) {
   document.write("<p style='color:green;'>DA</p>");
 } else {
   document.write(
-    `<p style='color:red;'>NE! Potrebno je da ${
+    `<p style='color:red;'>NE! Potrebno je da ${Math.ceil(
       n - v / 3
-    } coveka napuste lokal!</p>`
+    )} coveka napuste lokal!</p>`
   );
 }
